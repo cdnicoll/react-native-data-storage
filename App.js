61712b4dff7c0349bb0be6e5407afdc962522af3
cdnicoll <cdnicoll@gmail.com>
2019-03-06 15:18:43 -0800

    initial layout setup

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, Card, CardSection, Button } from './src/components/common';
 
 export default class App extends React.Component {
+  state = {
+    name: '',
+    age: undefined,
+  };
+  onNameChange = text => {
+    this.setState({ name: text });
+  };
+
+  onAgeChange = text => {
+    this.setState({ age: text });
+  };
+
+  onButtonPress = () => {
+    console.log(this.state.name, this.state.age);
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Card>
+          <CardSection>
+            <Input
+              label='Name'
+              placeholder='Name'
+              onChangeText={this.onNameChange.bind(this)}
+              value={this.state.name}
+            />
+          </CardSection>
+          <CardSection>
+            <Input
+              label='Age'
+              placeholder='20'
+              onChangeText={this.onAgeChange.bind(this)}
+              value={this.state.age}
+            />
+          </CardSection>
+          <CardSection>
+            <Button onPress={this.onButtonPress.bind(this)}>Save</Button>
+          </CardSection>
+        </Card>
       </View>
     );
   }
@@ -15,7 +52,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
   },
 });
